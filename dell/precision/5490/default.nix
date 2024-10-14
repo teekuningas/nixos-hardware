afe35f77814ffccec8c055e01652a8c6585b317e
Erkka Heinilä <erkkahe@gmail.com>
2024-10-14 11:00:05 +0300

    Fix

--- dell/precision/5490/default.nix
+++ dell/precision/5490/default.nix
@@ -7,7 +7,7 @@
   # boot.kernelParams = lib.mkIf (lib.versionOlder config.boot.kernelPackages.kernel "6.7")  [ "i915.force_probe=7d55" ];
   #boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.7") (lib.mkDefault pkgs.linuxPackages_latest);
 
-  boot.kernelParams = builtins.trace config.boot.kernelPackages.kernel [ "i915.force_probe=7d55" ];
+  boot.kernelParams = builtins.trace boot.kernelPackages.kernel [ "i915.force_probe=7d55" ];
 
   hardware.nvidia.prime = {
     intelBusId = "PCI:0:2:0";
