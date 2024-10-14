0459c75d71f99c49c749decf630e91cd2ff6b568
Erkka Heinilä <erkkahe@gmail.com>
2024-10-14 11:22:00 +0300

    Try fix

--- dell/precision/5490/default.nix
+++ dell/precision/5490/default.nix
@@ -1,13 +1,10 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   imports = [
     ../../../common/gpu/nvidia/ada-lovelace
   ];
 
-  # boot.kernelParams = lib.mkIf (lib.versionOlder config.boot.kernelPackages.kernel "6.7")  [ "i915.force_probe=7d55" ];
-  #boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.7") (lib.mkDefault pkgs.linuxPackages_latest);
-
-  boot.kernelParams = builtins.trace boot.kernelPackages.kernel [ "i915.force_probe=7d55" ];
+  boot.kernelParams = lib.mkIf (lib.versionOlder config.boot.kernelPackages.kernel.version "6.7") [ "i915.force_probe=7d55" ];
 
   hardware.nvidia.prime = {
     intelBusId = "PCI:0:2:0";
